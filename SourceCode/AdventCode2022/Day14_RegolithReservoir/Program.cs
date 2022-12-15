4c1a96610c3f0376a0c9b37f7e0b5ebd8722ed10
DCz <DCz@aucotec.com>
2022-12-15 19:03:47 +0100

    Day14_RegolithReservoir + Day15_BeaconExclusionZone

---
+++ SourceCode/AdventCode2022/Day14_RegolithReservoir/Program.cs
@@ -0,0 +1,183 @@
+﻿
+var input = File.ReadAllText($"Resources\\Example1.txt");
+
+var score = Part1(input, true);
+Console.WriteLine($"Result Part1: {score}");
+
+score = Part2(input, false);
+Console.WriteLine($"Result Part2: {score}");
+
+//Part 1 
+//How many units of sand come to rest before sand starts flowing into the abyss below?
+object Part1(string input, bool endlessVoidUnder) => GetScore(input, new(500, 0), true);
+
+//Part 2
+//How many units of sand come to rest?
+object Part2(string input, bool endlessVoidUnder) => GetScore(input, new(500, 0), false);
+
+//startPosition -> point of pouring sand into the cave
+object GetScore(string input, Point startPosition, bool endlessVoidUnder)
+{
+    var instructions = GetInstructionsDrawOfStones(input);
+
+    var map = GetMap(instructions, startPosition, endlessVoidUnder);
+    var score = CountSteps(map, startPosition);
+    return score;
+}
+
+List<List<Point>> GetInstructionsDrawOfStones(string input)
+{
+    Point SelectPosition(string input)
+    {
+        var temp = input.Split(",");
+        return new(int.Parse(temp[0]), int.Parse(temp[1]));
+    }
+
+    var instructions = input.Split(Environment.NewLine)
+                        .Select(x => x.Split(" -> ")
+                                      .Select(SelectPosition)
+                                      .ToList())
+                        .ToList();
+
+    return instructions;
+}
+
+int CountSteps(bool[][] map, Point startPosition)
+{
+    var count = 0;
+    while (true)
+    {
+        var (finish, newX, newY) = GetNextPositionSand(map, startPosition);
+        if (finish is true)
+            return newX + newY == 0 ? ++count : count; //Increase count 1 only if sand touches point of pouring 
+
+        map[newY][newX] = true;
+        count++;
+    }
+}
+
+(bool Finish, int X, int Y) GetNextPositionSand(bool[][] map, Point startPosition)
+{
+    int currentX = startPosition.X;
+    int currentY = startPosition.Y;
+
+    bool GoDown() => !map[currentY + 1][currentX];
+    bool GoLeft() => !map[currentY + 1][currentX - 1];
+    bool GoRight() => !map[currentY + 1][currentX + 1];
+
+    while (true)
+        try
+        {
+            if (GoDown())
+            {
+                currentY++;
+                continue;
+            }
+
+            if (GoLeft())
+            {
+                currentY++;
+                currentX--;
+                continue;
+            }
+
+            if (GoRight())
+            {
+                currentY++;
+                currentX++;
+                continue;
+            }
+
+            if (currentY == 0)
+            {
+                //Sand touches point of pouring [Part 2]
+                return (true, 0, 0);
+            }
+
+            break;
+        }
+        catch
+        {
+            //Sand fall into the endless void [Part 1]
+            return (true, -1, -1);
+        }
+
+    return (false, currentX, currentY);
+}
+
+//Generate map from instructions where are stones 
+//Use endlessVoidUnder if you don't want create infinite floor
+bool[][] GetMap(List<List<Point>> instructions, Point startPosition, bool endlessVoidUnder)
+{
+    var allPositions = instructions.SelectMany(x => x).ToList();
+    allPositions.Add(startPosition);
+
+    var xMax = allPositions.Max(m => m.X);
+    var xMin = allPositions.Min(m => m.X);
+    var yMax = allPositions.Max(m => m.Y);
+    var yMin = allPositions.Min(m => m.Y);
+    var w = xMax - xMin + 1;
+    var h = yMax - yMin + 1;
+
+    //Corigate the size map
+    //Only if required
+    if (!endlessVoidUnder)
+    {
+        h += 2;
+        xMin -= h;
+        xMax += h;
+        w = xMax - xMin + 1;
+    }
+
+    //Corigate the initial positions [Minimalize map]
+    for (int i = 0; i < allPositions.Count; i++)
+    {
+        var position = allPositions[i];
+        position.X -= xMin;
+        position.Y -= yMin;
+    }
+
+    //Initialize the map with size height x width
+    var map = new bool[h][];
+    foreach (var rowId in Enumerable.Range(0, h))
+        map[rowId] = new bool[w];
+
+    //Complete the map -> check where are stones
+    foreach (var instruction in instructions)
+        for (int i = 1; i < instruction.Count; i++)
+            foreach (var point in GetAllPositionsBetween(instruction[i - 1], instruction[i]))
+                map[point.Y][point.X] = true;
+
+    //Create infinite floor -> 2 lvls under last stone.  
+    //Only if required
+    if (!endlessVoidUnder)
+        for (int x = 0; x < w; x++)
+            map[h - 1][x] = true;
+
+    return map;
+}
+
+//Get all available positions between 2 positions (with these) 
+//Position1=5,0, Position2=7,0 -> 5,0 ; 6,0 ; 7;0
+static IEnumerable<Point> GetAllPositionsBetween(Point position1, Point position2)
+{
+    var minX = Math.Min(position1.X, position2.X);
+    var minY = Math.Min(position1.Y, position2.Y);
+    var maxX = Math.Max(position1.X, position2.X);
+    var maxY = Math.Max(position1.Y, position2.Y);
+
+    for (int x = minX; x <= maxX; x++)
+        for (int y = minY; y <= maxY; y++)
+            yield return new(x, y);
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ SourceCode/AdventCode2022/Day15_BeaconExclusionZone/Program.cs
@@ -0,0 +1,179 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText($"Resources\\Example1.txt");
+
+var score = Part1(input, 2000000);
+Console.WriteLine($"Result Part1: {score}");
+
+score = Part2(input, new Range(0, 4000000));
+Console.WriteLine($"Result Part2: {score}");
+
+// In the row where y=2000000, how many positions cannot contain a beacon?
+object Part1(string input, int yToScan)
+{
+    var scans = input.Split(Environment.NewLine)
+                     .Select(SelectScanInfo)
+                     .ToList();
+
+    var minX = int.MaxValue;
+    var maxX = int.MinValue;
+    var beacons = new HashSet<string>();
+    foreach (var scan in scans)
+    {
+        var (success, startX, endX, beaconKey) = scan.ScanFields(yToScan);
+        if (!success)
+            continue;
+
+        minX = Math.Min(minX, startX);
+        maxX = Math.Max(maxX, endX);
+        if (beaconKey is not null && !beacons.Contains(beaconKey))
+            beacons.Add(beaconKey);
+    }
+
+    var score = maxX - minX - beacons.Count + 1;
+    return score;
+}
+
+//What is its tuning frequency?
+object Part2(string input, Range rangeSearch)
+{
+    const ulong ratioFrequency = 4000000;
+
+    var scans = input.Split(Environment.NewLine)
+                     .Select(SelectScanInfo)
+                     .ToList();
+
+    foreach (var y in Enumerable.Range(rangeSearch.Start, rangeSearch.End)) 
+    {
+        var ranges = new Ranges();
+        foreach (var scan in scans)
+        {
+            var (success, startX, endX, _) = scan.ScanFields(y);
+            if (!success)
+                continue;
+
+            ranges.AddRange(new Range(startX, endX));
+        }
+
+        var x = ranges.GetFirstNumberOutsideRange(rangeSearch);
+        if (x.HasValue)
+            return (ulong)x.Value * ratioFrequency + (ulong)y;
+    }
+
+    return -1;
+}
+
+ScanInfo SelectScanInfo(string input)
+{
+    var regex = new Regex(@"\-?[0-9]+");
+    var matches = regex.Matches(input).Select(x => int.Parse(x.Value)).ToArray();
+
+    var sensorPosition = new Point(matches[0], matches[1]);
+    var beaconPosition = new Point(matches[2], matches[3]);
+    var scanInfo = new ScanInfo(sensorPosition, beaconPosition);
+
+    return scanInfo;
+}
+
+class ScanInfo
+{
+    public readonly int Range;
+
+    public Point SensorPosition { get; set; }
+    public Point BeaconPosition { get; set; }
+
+    public ScanInfo(Point sensorPosition, Point beaconPosition)
+    {
+        SensorPosition = sensorPosition;
+        BeaconPosition = beaconPosition;
+        Range = GetRange();
+    }
+
+    public int GetRange() => Math.Abs(BeaconPosition.X - SensorPosition.X) + Math.Abs(BeaconPosition.Y - SensorPosition.Y);
+
+    public (bool Success, int StartX, int EndX, string? BeaconKey) ScanFields(int y)
+    {
+        var minY = SensorPosition.Y - Range;
+        var maxY = SensorPosition.Y + Range;
+        var deltaY = Math.Abs(SensorPosition.Y - y);
+
+        if (y < minY || y > maxY)
+            return (false, 0, 0, null);
+
+        var startX = (SensorPosition.X - Range) + deltaY;
+        var endX = (SensorPosition.X + Range) - deltaY;
+        if (BeaconPosition.Y == y)
+            return (true, startX, endX, BeaconPosition.ToString());
+
+        return (true, startX, endX, null);
+    }
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString() => $"X:{X},Y:{Y}";
+}
+
+public class Ranges
+{
+    public List<Range> Items = new List<Range>();
+
+    //Add and merge with exist ranges
+    public void AddRange(Range range)
+    {
+        var rangesBefore = Items.Where(x => x.Start <= range.End);
+        var rangesAfter = Items.Where(x => x.End >= range.Start);
+
+        var ranges = rangesBefore.Intersect(rangesAfter).ToList();
+        foreach (var nextRange in ranges)
+            Items.Remove(nextRange);
+
+        ranges.Add(range);
+
+        var start = ranges.Min(x => x.Start);
+        var end = ranges.Max(x => x.End);
+
+        Items.Add(new Range(start, end));
+        Items = Items.OrderBy(x => x.Start).ToList();
+    }
+
+    //Required to Part2
+    //Get First number from param of range, which is not contained in this
+    public int? GetFirstNumberOutsideRange(Range range)
+    {
+        if (!Items.Any())
+            return null;
+
+        if (Items.Count == 1)
+        {
+            var single = Items.Single();
+            if (range.Start < single.Start)
+                return range.Start;
+            if (range.End > single.End)
+                return single.End;
+
+            return null;
+        }
+
+        return Items.First().End + 1;
+    }
+}
+
+public class Range
+{
+    public int Start { get; set; }
+    public int End { get; set; }
+
+    public Range(int start, int end)
+    {
+        Start = start;
+        End = end;
+    }
+}
